fff1947c5f84efc5de033c7b60cf2188f9864179
Steve <surrylic@yahoo.com>
2023-06-14 12:34:02 -0400

    updating stuff

--- main.tf
+++ main.tf
@@ -17,7 +17,25 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance-test-1.id]
     tags = {
       Name = "terraform-example"
     }
+    user_data = <<-EOF
+      #!/bin/bash
+      echo "Scott is a poopoo head" > index.html
+      nohup busybox httpd -f -p 8080 &
+      EOF
+    user_data_replace_on_change = true
+}
+
+resource "aws_security_group" "instance-test-1" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
