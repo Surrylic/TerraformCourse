1fc84f6ec6b543393477f10598c95f9c20955349
Steve <surrylic@yahoo.com>
2023-06-15 07:00:38 -0400

    Changing main to use variables from the variables.tf file.

--- main.tf
+++ main.tf
@@ -17,14 +17,15 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    public_ip = ${var.public_ip}
     vpc_security_group_ids = [aws_security_group.instance-test-1.id]
     tags = {
       Name = "terraform-example"
     }
     user_data = <<-EOF
       #!/bin/bash
-      echo "Scott is a poopoo head" > index.html
-      nohup busybox httpd -f -p 8080 &
+      echo "Mikey is a poopoo head" > index.html
+      nohup busybox httpd -f -p ${var.server_port} &
       EOF
     user_data_replace_on_change = true
 }
@@ -33,8 +34,8 @@ resource "aws_security_group" "instance-test-1" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = ${var.server_port}
+    to_port = ${var.server_port}
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
